7807f9d473a6832fae99d3c7291c28fcc40b3cd5
jnothan <jnothan@protonmail.com>
2020-10-02 10:03:35 +0300

    First part progress

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -9,11 +9,41 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Header text="give feedback"/>
+      <button onClick={() => setGood(good + 1)}>
+        good
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)}>
+        neutral
+      </button>
+      <button onClick={() => setBad(bad + 1)}>
+        bad
+      </button>
+      <Header text="statistics"/>
+      <Statistics/>
     </div>
   )
 }
 
+const Header = (props) => (
+  <div>
+    <h1>
+      {props.text}
+    </h1>
+  </div>
+)
+
+const Statistics = () => (
+  <div>
+    <p>
+      good<br/>
+      neutral<br/>
+      bad<br/>
+      all<br/>
+    </p>
+  </div>
+)
+
 ReactDOM.render(<App />, 
   document.getElementById('root')
-)
+) 
