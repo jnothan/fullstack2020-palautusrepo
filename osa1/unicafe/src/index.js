1763bf1e6ef6decb60586dcf8f4aca9cd0030f8b
jnothan <jnothan@protonmail.com>
2020-10-08 11:23:15 +0300

    Unicafe on valmis

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -2,25 +2,19 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const statesArray = [good, neutral, bad]
 
   return (
     <div>
-      <Header text="give feedback"/>
-      <button onClick={() => setGood(good + 1)}>
-        good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-        bad
-      </button>
-      <Header text="statistics"/>
-      <Statistics/>
+      <Header text="give feedback" />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Header text="statistics" />
+      <Statistics stats={statesArray} />
     </div>
   )
 }
@@ -33,17 +27,50 @@ const Header = (props) => (
   </div>
 )
 
-const Statistics = () => (
-  <div>
-    <p>
-      good<br/>
-      neutral<br/>
-      bad<br/>
-      all<br/>
-    </p>
-  </div>
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
 )
 
-ReactDOM.render(<App />, 
+const Statistics = (props) => {
+  const goodStat = props.stats[0]
+  const neutralStat = props.stats[1]
+  const badStat = props.stats[2]
+  const sum = goodStat + neutralStat + badStat
+  const mean = (goodStat - badStat) / sum
+  const positivePercentage = goodStat * 100 / sum + " %"
+
+  if (sum === 0) {
+    return (
+      <div>
+        <p>
+          No feedback given
+        </p>
+      </div>
+    )
+  }
+
+  return (
+    <table>
+      <tbody>
+        <tr><StatisticLine text="good" value={goodStat} /></tr>
+        <tr><StatisticLine text="neutral" value={neutralStat} /></tr>
+        <tr><StatisticLine text="bad" value={badStat} /></tr>
+        <tr><StatisticLine text="all" value={sum} /></tr>
+        <tr><StatisticLine text="average" value={mean} /></tr>
+        <tr><StatisticLine text="positive" value={positivePercentage} /></tr>
+      </tbody>
+    </table >
+  )
+}
+
+const StatisticLine = (props) => (
+  <>
+    <td>{props.text}</td>
+    <td>{props.value}</td>
+  </>
+)
+
+
+ReactDOM.render(<App />,
   document.getElementById('root')
 ) 
